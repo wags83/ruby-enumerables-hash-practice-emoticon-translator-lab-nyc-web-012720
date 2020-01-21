a23a8bff0cfad9a974ee50e96fb5a52f1beb148c
Greg Wagner <gpwagner@gmail.com>
2020-01-21 01:01:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,11 +8,26 @@ def load_library(file_path)
     'get_emoticon' => {}
   }
 
+  binding.pry
+
     emoticons.each do |key,value|
+      binding.pry
       new_hash['get_emoticon'][value[0]] = emoticons[key][1]
       new_hash['get_meaning'][value[1]] = key
     end
 
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+
+  new_hash = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
+
+    emoticons.each do |key,value|
+      new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+      new_hash['get_meaning'][value[1]] = key
+    end
     new_hash
   end
 
@@ -20,16 +35,17 @@ def load_library(file_path)
     result = load_library(file_path)['get_emoticon'][emoticon]
     if result != nil
       result
-    else
+    elsif result == nil
       "Sorry, that emoticon was not found"
     end
   end
+end
 
   def get_english_meaning(file_path, emoticon)
     result = load_library(file_path)['get_meaning'][emoticon]
     if result != nil
       result
-    else
+    elsif result == nil
       "Sorry, that emoticon was not found"
     end
   end
